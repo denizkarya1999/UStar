8543e314fdec531db4ba3f3184705524547ac332
Deniz K. Acikbas <karya99dkak@gmail.com>
2026-04-19 22:59:55 -0400

    <DKA - April 7th 2026> Updated DynaSpa denoising accuracy.

--- UStar/app/src/main/java/com/developer27/ustar/machinelearning/DynaSpa/MiniDynaSpaProcessor.kt
+++ UStar/app/src/main/java/com/developer27/ustar/machinelearning/DynaSpa/MiniDynaSpaProcessor.kt
@@ -21,7 +21,7 @@ object MiniDynaSpaPreprocessor {
 
     // Sensitivity to "heat" for bounding box extraction (0.0 to 1.0)
     // Lower = larger box, higher = tighter box
-    var globalThresholdRatio = 0.10f
+    var globalThresholdRatio = 0.01f
 
     // Hard-coded class labels
     const val CLASS_NO_UOID = 0
